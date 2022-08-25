717655512c03fd05af65e7ed653ed480c759ca04
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-08-25 14:44:21 +0200

    :sparkles: Add searchBar

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,10 +2,15 @@ import React, { FC } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.scss';
 import Navigation from "./mainFrames/navigation/Navigation";
+import {Container} from "react-bootstrap";
+import SearchBar from "./mainFrames/searchBar/SearchBar";
 
 const App: FC<Record<string, never>> = () => {
     return (
         <>
+            <Container>
+                <SearchBar />
+            </Container>
             <Navigation />
         </>
 )};
---
+++ frontend/src/components/mainFrames/searchBar/SearchBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './searchBar.scss';
+import { InputGroup, Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
+type SearchBarProps = {};
+
+const SearchBar: React.FunctionComponent<SearchBarProps> = () => {
+
+    return (
+        <div className="search-bar">
+            <InputGroup>
+                <InputGroup.Text>
+                    <FontAwesomeIcon
+                        icon={faSearch}
+                        className="search-bar__icon"
+                    />
+                </InputGroup.Text>
+                <Form.Control
+                />
+            </InputGroup>
+        </div>
+    );
+};
+
+SearchBar.displayName = "SearchBar";
+export default SearchBar;
