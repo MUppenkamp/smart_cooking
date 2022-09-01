448c1e7b4404c050080482918857a3d051e77c11
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-01 09:54:27 +0200

    :sparkles: Add noContent site

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,17 +1,27 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.scss';
 import Navigation from "./mainFrames/navigation/Navigation";
 import {Container} from "react-bootstrap";
 import SearchBar from "./mainFrames/searchBar/SearchBar";
+import NoContent from "./mainFrames/noContent/NoContent";
 
 const App: FC<Record<string, never>> = () => {
+    const [selectedNav, setSelectedNav] = useState(1);
+
     return (
         <>
             <Container>
                 <SearchBar />
+                <Container>
+                    {/* TODO Add render of sites here */}
+                    <NoContent />
+                </Container>
             </Container>
-            <Navigation />
+            <Navigation
+                selectedNav={selectedNav}
+                setSelectedNav={setSelectedNav}
+            />
         </>
 )};
 
