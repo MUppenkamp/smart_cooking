43c895bc194880d3849a248a8e5fba1fcde204e6
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-01 09:55:24 +0200

    :sparkles: Change state of selected nav item

--- frontend/src/components/mainFrames/navigation/Navigation.tsx
+++ frontend/src/components/mainFrames/navigation/Navigation.tsx
@@ -1,43 +1,63 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import './navigation.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faUtensils, faHeart, faCalendar, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import {faUtensils, faHeart, faCalendar, faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 
-type NavigationProps = {};
+type NavigationProps = {
+    selectedNav: number,
+    setSelectedNav: Dispatch<SetStateAction<number>>
+};
 
-const Navigation: React.FunctionComponent<NavigationProps> = () => {
+const Navigation: React.FunctionComponent<NavigationProps> = ({
+                                                                  selectedNav,
+                                                                  setSelectedNav
+                                                              }) => {
+
     return (
         <div className="navigation">
-            <div className="navigation__item">
+            <div
+                className="navigation__item"
+                onClick={() => setSelectedNav(1)}
+            >
                 <FontAwesomeIcon
                     icon={faUtensils}
-                    className="navigation__item__icon navigation__item__icon--active"
+                    className={`navigation__item__icon ${selectedNav === 1 ? "navigation__item__icon--active" : ""}`}
                 />
-                <p className="navigation__item__text navigation__item__text--active">Rezepte</p>
+                <p className={`navigation__item__text ${selectedNav === 1 ? "navigation__item__text--active" : ""}`}>Rezepte</p>
             </div>
-            <div className="navigation__item">
+            <div
+                className="navigation__item"
+                onClick={() => setSelectedNav(2)}
+            >
                 <FontAwesomeIcon
                     icon={faHeart}
-                    className="navigation__item__icon"
+                    className={`navigation__item__icon ${selectedNav === 2 ? "navigation__item__icon--active" : ""}`}
                 />
-                <p className="navigation__item__text">Favoriten</p>
+                <p className={`navigation__item__text ${selectedNav === 2 ? "navigation__item__text--active" : ""}`}>Favoriten</p>
             </div>
-            <div className="navigation__item">
+            <div
+                className="navigation__item"
+                onClick={() => setSelectedNav(3)}
+            >
                 <FontAwesomeIcon
                     icon={faCalendar}
-                    className="navigation__item__icon"
+                    className={`navigation__item__icon ${selectedNav === 3 ? "navigation__item__icon--active" : ""}`}
                 />
-                <p className="navigation__item__text">Wochenplan</p>
+                <p className={`navigation__item__text ${selectedNav === 3 ? "navigation__item__text--active" : ""}`}>Wochenplan</p>
             </div>
-            <div className="navigation__item">
+            <div
+                className="navigation__item"
+                onClick={() => setSelectedNav(4)}
+            >
                 <FontAwesomeIcon
                     icon={faShoppingCart}
-                    className="navigation__item__icon"
+                    className={`navigation__item__icon ${selectedNav === 4 ? "navigation__item__icon--active" : ""}`}
                 />
-                <p className="navigation__item__text">Einkaufliste</p>
+                <p className={`navigation__item__text ${selectedNav === 4 ? "navigation__item__text--active" : ""}`}>Einkaufliste</p>
             </div>
         </div>
-)};
+    )
+};
 
 Navigation.displayName = 'Navigation';
 export default Navigation;
