95813fc19543f69bd8f5fa48e9f389230a2d0328
Jana <jana.walfort@tobit.com>
2022-09-01 13:04:21 +0200

    :sparkles: Add selectors

--- frontend/src/redux/recipeWeek/recipeWeekSelectors.ts
+++ frontend/src/redux/recipeWeek/recipeWeekSelectors.ts
@@ -2,6 +2,8 @@ import type { RootState } from "../store";
 
 export const selectRecipeWeekFetchState = (state: RootState) => state.recipesWeek.fetchState;
 
-export const selectRecipeWeek = (state: RootState) => state.recipesWeek.data;
+export const selectRecipeWeekIds = (state: RootState) => state.recipesWeek.data.ids;
+
+export const selectRecipeWeekEntities = (state: RootState) => state.recipesWeek.data.entities;
 
 export const selectRecipeWeekDay = (state: RootState, date: string) => state.recipesWeek.data.entities[date];
