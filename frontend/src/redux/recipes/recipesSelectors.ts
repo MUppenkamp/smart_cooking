0af85e37110e8da1dc42c4532f1d1ab5abdf6244
Jana <jana.walfort@tobit.com>
2022-09-07 14:32:53 +0200

    :construction: Update recipe selectors

--- frontend/src/redux/recipes/recipesSelectors.ts
+++ frontend/src/redux/recipes/recipesSelectors.ts
@@ -1,8 +1,13 @@
 import type { RootState } from '../store';
+import { recipesAdapter } from './recipesSlice';
+import { EntityId } from '@reduxjs/toolkit';
+
+const recipesSelector = recipesAdapter.getSelectors((state: RootState) => state.favoriteRecipes.data);
 
 export const selectRecipesFetchState = (state: RootState) => state.recipes.fetchState;
 
-export const selectRecipesIds = (state: RootState) => state.recipes.data.ids;
+export const selectRecipesIds = (state: RootState) => recipesSelector.selectIds(state);
 
-export const selectRecipes = (state: RootState) => state.recipes.data.entities;
+export const selectRecipes = (state: RootState) => recipesSelector.selectAll(state);
 
+export const selectRecipe = (state: RootState, id: EntityId) => recipesSelector.selectById(state, id);
