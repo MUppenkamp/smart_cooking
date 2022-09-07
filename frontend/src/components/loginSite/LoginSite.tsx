d8f8718aea28f4acad6e263da54a82a1a1cf2ff8
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-07 14:41:47 +0200

    :sparkles: Add login site

--- frontend/src/components/loginSite/LoginSite.tsx
+++ frontend/src/components/loginSite/LoginSite.tsx
@@ -6,18 +6,29 @@ type LoginSiteProps = {};
 
 const LoginSite: React.FunctionComponent<LoginSiteProps> = () => {
     const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <>
+            <p>Login</p>
             <InputGroup className="mb-3">
                 <Form.Control
-                    value=''
+                    id='loginEmail'
+                    className='login__input'
+                    value={mail}
+                    onChange={(value) => {
+                        setMail(value.target.value);
+                    }}
                     placeholder='E-Mail'
                 />
             </InputGroup>
             <InputGroup className="mb-3">
                 <Form.Control
-                    value=''
+                    className='login__input'
+                    value={password}
+                    onChange={(value) => {
+                        setPassword(value.target.value);
+                    }}
                     placeholder='Passwort'
                 />
             </InputGroup>
