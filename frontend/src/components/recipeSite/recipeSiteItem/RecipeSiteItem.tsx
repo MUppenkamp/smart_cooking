259c8aa0c8d55d177804ae4fe84cc331f52582cb
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-07 12:31:19 +0200

    🏷️ Change type

--- frontend/src/components/recipeSite/recipeSiteItem/RecipeSiteItem.tsx
+++ frontend/src/components/recipeSite/recipeSiteItem/RecipeSiteItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import './recipeSiteItem.scss';
 import {TRecipe} from "../../../types/recipe";
 import {Image} from "react-bootstrap";
@@ -6,15 +6,20 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHeart} from "@fortawesome/free-solid-svg-icons";
 
 type RecipeSiteItemProps = {
-    recipe: TRecipe
+    recipe: TRecipe,
+    setSelectedRecipe: Dispatch<SetStateAction<TRecipe | null>>
 };
 
 const RecipeSiteItem: React.FunctionComponent<RecipeSiteItemProps> = ({
-                                                                          recipe
+                                                                          recipe,
+                                                                          setSelectedRecipe
                                                                       }) => {
 
     return (
-        <div className='recipe-site-item__recipe'>
+        <div
+            className='recipe-site-item__recipe'
+            onClick={() => setSelectedRecipe(recipe)}
+        >
             <div className='recipe-site-item'>
                 <div className='recipe-site-item__icon'>
                     <FontAwesomeIcon
