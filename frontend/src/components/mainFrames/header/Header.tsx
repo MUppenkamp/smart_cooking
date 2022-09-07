25a18249636c01215e988b9d2b922d3396f921e5
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-07 14:07:15 +0200

    :sparkles: Add selectedSite enum

--- frontend/src/components/mainFrames/header/Header.tsx
+++ frontend/src/components/mainFrames/header/Header.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import './header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { SelectedSite } from '../../../constants/selectedSite';
 
 
-type HeaderProps = {};
+type HeaderProps = {
+    setSelectedSite: Dispatch<SetStateAction<SelectedSite | null>>
+};
 
-const Header: React.FunctionComponent<HeaderProps> = () => {
+const Header: React.FunctionComponent<HeaderProps> = ({
+                                                          setSelectedSite
+                                                      }) => {
 
     return (
-        <div className="header">
+        <div
+            className='header'
+            // TODO isLoggedIn ? setSelectedSite.SETTINGS_SITE : setSelectedSite.LOGIN_SITE
+            onClick={() => setSelectedSite(SelectedSite.LOGIN_SITE)}
+        >
             {/*<Image*/}
             {/*    src='images/logo.png'*/}
             {/*    fluid*/}
             {/*    width={50}*/}
             {/*/>*/}
             <h1>LOGO</h1>
-            <div className="header__border">
+            <div className='header__border'>
                 <FontAwesomeIcon
                     icon={faUser}
-                    className="header__border__icon"
+                    className='header__border__icon'
                 />
             </div>
         </div>
