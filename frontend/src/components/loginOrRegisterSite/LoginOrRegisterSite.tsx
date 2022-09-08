7c9887650a2c59df4cc40d00e26d66be4e102866
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-08 14:32:38 +0200

    :lipstick: Outsource sytles for input

--- frontend/src/components/loginOrRegisterSite/LoginOrRegisterSite.tsx
+++ frontend/src/components/loginOrRegisterSite/LoginOrRegisterSite.tsx
@@ -38,7 +38,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     <p>Login</p>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={mail}
                             onChange={(value) => {
                                 setMail(value.target.value);
@@ -48,7 +48,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     </InputGroup>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={password}
                             onChange={(value) => {
                                 setPassword(value.target.value);
@@ -66,7 +66,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     <p>Registrieren</p>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={firstName}
                             onChange={(value) => {
                                 setFirstName(value.target.value);
@@ -76,7 +76,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     </InputGroup>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={lastName}
                             onChange={(value) => {
                                 setLastName(value.target.value);
@@ -86,7 +86,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     </InputGroup>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={mail}
                             onChange={(value) => {
                                 setMail(value.target.value);
@@ -96,7 +96,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     </InputGroup>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={password}
                             onChange={(value) => {
                                 setPassword(value.target.value);
@@ -107,7 +107,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     </InputGroup>
                     <InputGroup className='mb-3'>
                         <Form.Control
-                            className='login-or-register__input'
+                            className='login-or-register__input input'
                             value={passwordRepeat}
                             onChange={(value) => {
                                 setPasswordRepeat(value.target.value);
