4628fadacbf9822e2f157db05a21ed447d1e976b
Jana <jana.walfort@tobit.com>
2022-09-08 14:09:28 +0200

    :construction: Update requestHelper

--- frontend/src/utils/requestHelper.ts
+++ frontend/src/utils/requestHelper.ts
@@ -1,7 +1,16 @@
 import { THelper, THelperParams } from '../types/requestHelper';
 
-const requestHelper = async <T>({ requestUrl, options }: THelperParams): Promise<THelper<T>> => {
-    const response = await fetch(requestUrl, options);
+const requestHelper = async <T>({requestUrl, options}: THelperParams): Promise<THelper<T>> => {
+    const response = await fetch(requestUrl, {
+        ...options,
+        headers: {
+            // @ts-ignore
+            ...options?.headers,
+            'Content-Type': 'application/json'
+        },
+        // @ts-ignore
+        body: (options?.body ? JSON.stringify(options.body) : null)
+    });
 
     if (response.status !== 200) {
         return {
