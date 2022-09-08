e37d01c37477c2bacfad211a319b892e294ea0ba
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-09-08 11:11:52 +0200

    :lipstick: Add cute svg

--- frontend/src/components/loginOrRegisterSite/LoginOrRegisterSite.tsx
+++ frontend/src/components/loginOrRegisterSite/LoginOrRegisterSite.tsx
@@ -104,7 +104,7 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                 </>
             );
         }
-    }
+    };
 
     const renderButtonLogin = () => {
         return (
@@ -118,8 +118,8 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     Einloggen
                 </Button>
             </div>
-        )
-    }
+        );
+    };
 
     const renderButtonRegister = () => {
         return (
@@ -133,24 +133,47 @@ const LoginOrRegisterSite: React.FunctionComponent<LoginSiteProps> = ({
                     Registrieren
                 </Button>
             </div>
-        )
-    }
+        );
+    };
 
     return (
-        <div className='login-or-register'>
-            {renderInputs()}
-            {
-                login ? renderButtonLogin() : renderButtonRegister()
-            }
-            <div className='login-or-register__or'>
-                <div className='login-or-register__or__1'></div>
-                <span className='login-or-register__or__2'>oder</span>
-                <div className='login-or-register__or__3'></div>
+        <>
+            <svg id='visual' viewBox='0 0 900 600' width='900' height='600'>
+                <path
+                    className='svg-path'
+                    d='M0 357L30 354.7C60 352.3 120 347.7 180 349.2C240 350.7 300 358.3 360 340.8C420 323.3 480 280.7 540 280.5C600 280.3 660 322.7 720 332.7C780 342.7 840 320.3 870 309.2L900 298L900 0L870 0C840 0 780 0 720 0C660 0 600 0 540 0C480 0 420 0 360 0C300 0 240 0 180 0C120 0 60 0 30 0L0 0Z'
+                    fill='#ace08b' stroke-linecap='round' stroke-linejoin='miter'
+                >
+                </path>
+            </svg>
+            <div className='login-or-register'>
+                <h3 className='login-or-register__greeting'>
+                    <p className='login-or-register__greeting__1'>
+                        {
+                            login ? 'Willkommen' : 'Account'
+                        }
+                    </p>
+                    <p>
+                        {
+
+                            login ? 'Zurück' : 'Erstellen'
+                        }
+                    </p>
+                </h3>
+                {renderInputs()}
+                {
+                    login ? renderButtonLogin() : renderButtonRegister()
+                }
+                <div className='login-or-register__or'>
+                    <div className='login-or-register__or__1'></div>
+                    <span className='login-or-register__or__2'>oder</span>
+                    <div className='login-or-register__or__3'></div>
+                </div>
+                {
+                    login ? renderButtonRegister() : renderButtonLogin()
+                }
             </div>
-            {
-                login ? renderButtonRegister() : renderButtonLogin()
-            }
-        </div>
+        </>
     );
 };
 
