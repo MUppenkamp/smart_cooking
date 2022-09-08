0931e885d7cefae83e2dafe72f11157cc6dcffd1
Noah <noah.dahlhaus@tobit.com>
2022-09-08 11:04:10 +0200

    🐛 Fix duplicate data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ const createRecipeTable = 'CREATE TABLE IF NOT EXISTS recipe (id serial primary 
 const createUser2RecipeTable = 'CREATE TABLE IF NOT EXISTS user_2_recipe (user_id integer references app_user(id), recipe_id integer references recipe(id), is_favorite boolean, is_own boolean)';
 const createRecipeWeekDayTable = 'CREATE TABLE IF NOT EXISTS recipe_week_day (id serial primary key, recipe_week_id integer references recipe_week(id), day_date date, recipe_id integer references recipe(id))';
 const createQuantityUnitTable = 'CREATE TABLE IF NOT EXISTS quantity_unit (id serial primary key, name varchar UNIQUE)';
-const createRecipeIngredientTable = 'CREATE TABLE IF NOT EXISTS recipe_ingredient (id serial primary key, recipe_id integer references recipe(id), name varchar, quantity integer, quantity_unit_id integer references quantity_unit(id))';
+const createRecipeIngredientTable = 'CREATE TABLE IF NOT EXISTS recipe_ingredient (id serial primary key, recipe_id integer references recipe(id), name varchar, quantity decimal, quantity_unit_id integer references quantity_unit(id))';
 const createRecipeWeekDayShoppingList = 'CREATE TABLE IF NOT EXISTS recipe_week_day_shopping_list (id serial primary key, is_checked boolean, recipe_week_day_id integer references recipe_week_day(id), recipe_ingredient integer references recipe_ingredient(id))';
 
 // endregion
@@ -146,275 +146,279 @@ const createInitData = async () => {
 
     // region CreateDefaultRecipe
 
-    await createRecipe(
-        pool,
-        "Penne mit Paprika-Walnuss-Sauce",
-        20,
-        2,
-        "Penne mit Sauce // TODO",
-        4,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/569f6961-9d2c-462d-b48a-366c47a3e8f7/Derivates/684e5124-e59c-4d18-959b-7dc627c769af.jpg',
-        [{
-            name: "Petersilie",
-            quantity: 0.5,
-            quantityId: quantity.bund
-        }, {
-            name: 'Wasser',
-            quantityId: 1,
-            quantity: 1500
-        }, {
-            name: "Salz",
-            quantity: 2.75,
-            quantityId: 4
-        }, {
-            name: "Spitzpaprika",
-            quantityId: 1,
-            quantity: 300
-        }, {
-            name: "Knoblauchzehen",
-            quantity: 3,
-            quantityId: quantity.anzahl
-        }, {
-            name: "Walnusshälften",
-            quantity: 80,
-            quantityId: quantity.g
-        }, {
-            name: "Penne",
-            quantity: 500,
-            quantityId: quantity.g
-        }, {
-            name: "Olivenöl",
-            quantity: 20,
-            quantityId: quantity.g
-        }, {
-            name: "Biber Salcasi (Paprikamark)",
-            quantity: 2,
-            quantityId: quantity.tl
-        }, {
-            name: "Oregano",
-            quantity: 1,
-            quantityId: quantity.tl
-        }, {
-            name: "Paprika rosenscharf",
-            quantity: 0.25,
-            quantityId: quantity.tl
-        }],
-        637,
-        19,
-        19
-    );
+    const selectRecipe = 'select * from recipe';
 
-    await createRecipe(
-        pool,
-        "Dattel-Curry-Dip",
-        5,
-        1,
-        "Dattel-Curry-Dip",
-        12,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/B3296F17-ACC5-4093-B868-D907693BBB85/Derivates/F99BFC6E-ADE7-4F80-8988-8BD07CC6A256.jpg',
-        [{
-            name: "Datteln",
-            quantity: 150,
-            quantityId: quantity.g
-        }, {
-            name: "Knoblauchzehen",
-            quantity: 1,
-            quantityId: quantity.anzahl
-        }, {
-            name: "Frischkäse",
-            quantity: 300,
-            quantityId: quantity.g
-        }, {
-            name: "Schmand",
-            quantity: 200,
-            quantityId: quantity.g
-        }, {
-            name: "Curry",
-            quantity: 2,
-            quantityId: quantity.tl
-        }, {
-            name: "Salz",
-            quantity: 0.5,
-            quantityId: quantity.tl
-        }, {
-            name: "Cayenne-Pfeffer",
-            quantity: 2,
-            quantityId: quantity.prise
-        }],
-    );
+    const recipe = await pgQuery(pool, selectRecipe);
 
-    await createRecipe(
-        pool,
-        "Erdnussbutter",
-        10,
-        1,
-        "Erdnussbutter",
-        1,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/D661D6E1-FD26-40D7-A048-D6743FC946C7/Derivates/532A16BD-C057-42F7-8493-9797B3FAA06E.jpg',
-        [{
-            name: "Erdnüsse",
-            quantity: 300,
-            quantityId: quantity.g
-        }, {
-            name: "Erdnussöl",
-            quantity: 20,
-            quantityId: quantity.g
-        }, {
-            name: "Honig",
-            quantity: 1.5,
-            quantityId: quantity.tl
-        }, {
-            name: "Salz",
-            quantity: 0.25,
-            quantityId: quantity.tl
-        }],
-    );
+    if (!recipe?.rowCount || recipe.rowCount <= 0) {
+        await createRecipe(
+            pool,
+            'Penne mit Paprika-Walnuss-Sauce',
+            20,
+            2,
+            'Penne mit Sauce // TODO',
+            4,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/569f6961-9d2c-462d-b48a-366c47a3e8f7/Derivates/684e5124-e59c-4d18-959b-7dc627c769af.jpg',
+            [{
+                name: 'Petersilie',
+                quantity: 0.5,
+                quantityId: quantity.bund
+            }, {
+                name: 'Wasser',
+                quantityId: 1,
+                quantity: 1500
+            }, {
+                name: 'Salz',
+                quantity: 2.75,
+                quantityId: 4
+            }, {
+                name: 'Spitzpaprika',
+                quantityId: 1,
+                quantity: 300
+            }, {
+                name: 'Knoblauchzehen',
+                quantity: 3,
+                quantityId: quantity.anzahl
+            }, {
+                name: 'Walnusshälften',
+                quantity: 80,
+                quantityId: quantity.g
+            }, {
+                name: 'Penne',
+                quantity: 500,
+                quantityId: quantity.g
+            }, {
+                name: 'Olivenöl',
+                quantity: 20,
+                quantityId: quantity.g
+            }, {
+                name: 'Biber Salcasi (Paprikamark)',
+                quantity: 2,
+                quantityId: quantity.tl
+            }, {
+                name: 'Oregano',
+                quantity: 1,
+                quantityId: quantity.tl
+            }, {
+                name: 'Paprika rosenscharf',
+                quantity: 0.25,
+                quantityId: quantity.tl
+            }],
+            637,
+            19,
+            19
+        );
 
-    await createRecipe(
-        pool,
-        "Curry-Cashews",
-        30,
-        1,
-        "Curry-Cashews",
-        8,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/9E953F3C-6E5C-4057-AE36-FB04D7D6F216/Derivates/628ef1ad-498b-4903-80c7-4d69d0026cc4.jpg',
-        [{
-            name: "Eiweiß",
-            quantity: 1,
-            quantityId: quantity.anzahl
-        }, {
-            name: "Curry, mild",
-            quantity: 4,
-            quantityId: quantity.tl
-        }, {
-            name: "Paprika de la Vera",
-            quantity: 0.5,
-            quantityId: quantity.tl
-        }, {
-            name: "Brauner Zucker",
-            quantity: 2,
-            quantityId: quantity.el
-        }, {
-            name: "Salz",
-            quantity: 2,
-            quantityId: quantity.tl
-        }, {
-            name: "Spritzer Tabasco",
-            quantity: 3,
-            quantityId: quantity.anzahl
-        }, {
-            name: "Cashewkerne, ungeröstet",
-            quantity: 400,
-            quantityId: quantity.g
-        }],
-    );
+        await createRecipe(
+            pool,
+            'Dattel-Curry-Dip',
+            5,
+            1,
+            'Dattel-Curry-Dip',
+            12,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/B3296F17-ACC5-4093-B868-D907693BBB85/Derivates/F99BFC6E-ADE7-4F80-8988-8BD07CC6A256.jpg',
+            [{
+                name: 'Datteln',
+                quantity: 150,
+                quantityId: quantity.g
+            }, {
+                name: 'Knoblauchzehen',
+                quantity: 1,
+                quantityId: quantity.anzahl
+            }, {
+                name: 'Frischkäse',
+                quantity: 300,
+                quantityId: quantity.g
+            }, {
+                name: 'Schmand',
+                quantity: 200,
+                quantityId: quantity.g
+            }, {
+                name: 'Curry',
+                quantity: 2,
+                quantityId: quantity.tl
+            }, {
+                name: 'Salz',
+                quantity: 0.5,
+                quantityId: quantity.tl
+            }, {
+                name: 'Cayenne-Pfeffer',
+                quantity: 2,
+                quantityId: quantity.prise
+            }],
+        );
 
+        await createRecipe(
+            pool,
+            'Erdnussbutter',
+            10,
+            1,
+            'Erdnussbutter',
+            1,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/D661D6E1-FD26-40D7-A048-D6743FC946C7/Derivates/532A16BD-C057-42F7-8493-9797B3FAA06E.jpg',
+            [{
+                name: 'Erdnüsse',
+                quantity: 300,
+                quantityId: quantity.g
+            }, {
+                name: 'Erdnussöl',
+                quantity: 20,
+                quantityId: quantity.g
+            }, {
+                name: 'Honig',
+                quantity: 1.5,
+                quantityId: quantity.tl
+            }, {
+                name: 'Salz',
+                quantity: 0.25,
+                quantityId: quantity.tl
+            }],
+        );
 
-    await createRecipe(
-        pool,
-        "Butterbier",
-        10,
-        1,
-        "Butterbier",
-        6,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/82d1ad3d-c6fb-4579-8bf8-65282a3d6d83/Derivates/93f9df5b-cd0e-413e-927e-bcbbff874137.jpg',
-        [{
-            name: "Malzbier",
-            quantity: 500,
-            quantityId: quantity.g
-        }, {
-            name: "Brauner Zucker",
-            quantity: 30,
-            quantityId: quantity.g
-        }, {
-            name: "Eigelb",
-            quantity: 4,
-            quantityId: quantity.anzahl
-        }, {
-            name: "Ingwer, gemahlen",
-            quantity: 0.5,
-            quantityId: quantity.tl
-        }, {
-            name: "Muskat",
-            quantity: 0.5,
-            quantityId: quantity.tl
-        }, {
-            name: "Zimt",
-            quantity: 1,
-            quantityId: quantity.tl
-        }, {
-            name: "Butter",
-            quantity: 30,
-            quantityId: quantity.g
-        }],
-    );
-
-    await createRecipe(
-        pool,
-        "Gemüse-Curry mit Couscous",
-        30,
-        1,
-        "Gemüse-Curry mit Couscous",
-        4,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/874BB5BD-5418-48F8-BAEC-FDABB57776A0/Derivates/D208C611-6991-477F-9775-6322F2E713E1.jpg',
-        [{
-            name: "Malzbier",
-            quantity: 500,
-            quantityId: quantity.g
-        }],
-    );
+        await createRecipe(
+            pool,
+            'Curry-Cashews',
+            30,
+            1,
+            'Curry-Cashews',
+            8,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/9E953F3C-6E5C-4057-AE36-FB04D7D6F216/Derivates/628ef1ad-498b-4903-80c7-4d69d0026cc4.jpg',
+            [{
+                name: 'Eiweiß',
+                quantity: 1,
+                quantityId: quantity.anzahl
+            }, {
+                name: 'Curry, mild',
+                quantity: 4,
+                quantityId: quantity.tl
+            }, {
+                name: 'Paprika de la Vera',
+                quantity: 0.5,
+                quantityId: quantity.tl
+            }, {
+                name: 'Brauner Zucker',
+                quantity: 2,
+                quantityId: quantity.el
+            }, {
+                name: 'Salz',
+                quantity: 2,
+                quantityId: quantity.tl
+            }, {
+                name: 'Spritzer Tabasco',
+                quantity: 3,
+                quantityId: quantity.anzahl
+            }, {
+                name: 'Cashewkerne, ungeröstet',
+                quantity: 400,
+                quantityId: quantity.g
+            }],
+        );
 
 
-    await createRecipe(
-        pool,
-        "Bunte Gemüsenudeln mit Pesto",
-        45,
-        2,
-        "Bunte Gemüsenudeln mit Pesto",
-        4,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/408A2A78-6FCA-46B5-8C39-A867FF4B969A/Derivates/d2e15b4f-c362-40bc-b355-104d4f605253.jpg',
-        [{
-            name: "Malzbier",
-            quantity: 500,
-            quantityId: quantity.g
-        }],
-    );
+        await createRecipe(
+            pool,
+            'Butterbier',
+            10,
+            1,
+            'Butterbier',
+            6,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/82d1ad3d-c6fb-4579-8bf8-65282a3d6d83/Derivates/93f9df5b-cd0e-413e-927e-bcbbff874137.jpg',
+            [{
+                name: 'Malzbier',
+                quantity: 500,
+                quantityId: quantity.g
+            }, {
+                name: 'Brauner Zucker',
+                quantity: 30,
+                quantityId: quantity.g
+            }, {
+                name: 'Eigelb',
+                quantity: 4,
+                quantityId: quantity.anzahl
+            }, {
+                name: 'Ingwer, gemahlen',
+                quantity: 0.5,
+                quantityId: quantity.tl
+            }, {
+                name: 'Muskat',
+                quantity: 0.5,
+                quantityId: quantity.tl
+            }, {
+                name: 'Zimt',
+                quantity: 1,
+                quantityId: quantity.tl
+            }, {
+                name: 'Butter',
+                quantity: 30,
+                quantityId: quantity.g
+            }],
+        );
+
+        await createRecipe(
+            pool,
+            'Gemüse-Curry mit Couscous',
+            30,
+            1,
+            'Gemüse-Curry mit Couscous',
+            4,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/874BB5BD-5418-48F8-BAEC-FDABB57776A0/Derivates/D208C611-6991-477F-9775-6322F2E713E1.jpg',
+            [{
+                name: 'Malzbier',
+                quantity: 500,
+                quantityId: quantity.g
+            }],
+        );
 
 
-    await createRecipe(
-        pool,
-        "Überbackenes Baguette",
-        45,
-        2,
-        "Überbackenes Baguette",
-        4,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/908f2ba0-d619-426e-ba6f-68e20372a680/Derivates/6e0ae93f-9635-4f30-97d2-4892b836ee31.jpg',
-        [{
-            name: "Malzbier",
-            quantity: 500,
-            quantityId: quantity.g
-        }],
-    );
+        await createRecipe(
+            pool,
+            'Bunte Gemüsenudeln mit Pesto',
+            45,
+            2,
+            'Bunte Gemüsenudeln mit Pesto',
+            4,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/408A2A78-6FCA-46B5-8C39-A867FF4B969A/Derivates/d2e15b4f-c362-40bc-b355-104d4f605253.jpg',
+            [{
+                name: 'Malzbier',
+                quantity: 500,
+                quantityId: quantity.g
+            }],
+        );
 
 
-    await createRecipe(
-        pool,
-        "Tomaten-Knoblauch-Butter",
-        20,
-        1,
-        "Tomaten-Knoblauch-Butter",
-        8,
-        'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/3afb29c1-4f19-4c23-b749-ca65c1bb0ea1/Derivates/e55d2fdb-a154-47fb-877c-a3fcf2762cd5.jpg',
-        [{
-            name: "Malzbier",
-            quantity: 500,
-            quantityId: quantity.g
-        }],
-    );
+        await createRecipe(
+            pool,
+            'Überbackenes Baguette',
+            45,
+            2,
+            'Überbackenes Baguette',
+            4,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/908f2ba0-d619-426e-ba6f-68e20372a680/Derivates/6e0ae93f-9635-4f30-97d2-4892b836ee31.jpg',
+            [{
+                name: 'Malzbier',
+                quantity: 500,
+                quantityId: quantity.g
+            }],
+        );
+
+
+        await createRecipe(
+            pool,
+            'Tomaten-Knoblauch-Butter',
+            20,
+            1,
+            'Tomaten-Knoblauch-Butter',
+            8,
+            'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/3afb29c1-4f19-4c23-b749-ca65c1bb0ea1/Derivates/e55d2fdb-a154-47fb-877c-a3fcf2762cd5.jpg',
+            [{
+                name: 'Malzbier',
+                quantity: 500,
+                quantityId: quantity.g
+            }],
+        );
+    }
 
     // endregion
-
-
 
     // endregion
 
@@ -423,10 +427,6 @@ const createInitData = async () => {
         setTimeout(async () => {
             console.log('create user');
             const pool2 = createPool();
-
-
-
-
 
             // region DefaultData
             await createData(pool2, 'Noah', 'Dahlhaus', 'noah@dahlhaus-online.de', 'Nudeln', 'Nudeln mit Nudeln', [{
