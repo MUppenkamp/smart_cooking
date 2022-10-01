59df8cdb831161faaa8e2fdfcb52f6caed677aa6
Mona <mona.uppi@gmx.de>
2022-10-02 00:36:39 +0200

    :fire: Remove passwordhelper

--- frontend/src/utils/passwordHelper.ts
+++
@@ -1,7 +0,0 @@
-export const hidePassword = (password: string) => {
-    return password.replace(/./g, '*');
-}
-
-export const saveHiddenPassword = (currPassword: string, changedPassword: string) => {
-    return (currPassword || '') + changedPassword[changedPassword.length - 1]
-}
